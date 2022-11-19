93fc8e938f94d1236f0aa83da147a5032223746f
yukost <yuratinkalyuk08@gmail.com>
2022-11-19 13:29:33 +0200

    bags

--- src/index.js
+++ src/index.js
@@ -1,16 +1,2 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-    body: document.querySelector('body'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
-  }
-})();
+import('./js/modal.js');
+import('./js/menu.js');
